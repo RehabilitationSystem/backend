f9eb56efd57a3bf00170cc11bca98ce5b81d331c
Zhang <2939597933@qq.com>
2023-11-20 20:37:58 +0800

    1.3.1（新闻模块和登录用户模块合并）

--- News_Control/src/main/java/com/example/news_control/controller/NewsController.java
+++ News_Control/src/main/java/com/example/news_control/controller/NewsController.java
@@ -1,5 +1,6 @@
 package com.example.news_control.controller;
 
+import com.example.commons.UnInterception;
 import com.example.news_control.entity.News;
 import com.example.news_control.service.NewsService;
 import jakarta.annotation.Resource;
@@ -16,11 +17,13 @@ public class NewsController {
     @Autowired
     NewsService newsService;
     @RequestMapping("/hello")
+    @UnInterception
     public String sayHello(){
         return "hello";
     }
 
     @PostMapping("/getById")
+    @UnInterception
     public News getNews(int new_id){
 //        newsService.getNews(new_id);
         return newsService.getNews(new_id);
