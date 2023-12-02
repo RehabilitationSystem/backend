9c5d568be82ecaab853aab43ef76d4dea5dedaa2
吴迪 <1547894573@qq.com>
2023-12-02 15:25:41 +0800

    0.0.2

--- comment_control/src/main/java/com/example/comment_control/service/CommentService.java
+++ comment_control/src/main/java/com/example/comment_control/service/CommentService.java
@@ -7,7 +7,7 @@ import com.example.comment_control.entity.Comment;
 import java.util.List;
 
 public interface CommentService {
-    void addComment(Comment commment);
+    void addComment(Comment comment);
     List<Comment> listAll();
     Comment findById(String id);
 }
