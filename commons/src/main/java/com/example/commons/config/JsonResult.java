6ba8d5d2ce7c625377c06e974a0013274d2812ea
liu <2206221643@qq.com>
2023-12-08 09:30:54 +0800

    修改成功请求的返回值为200

--- commons/src/main/java/com/example/commons/config/JsonResult.java
+++ commons/src/main/java/com/example/commons/config/JsonResult.java
@@ -6,10 +6,10 @@ public class JsonResult<T> {
     private String msg;
 
     /**
-     * 若没有数据返回，默认状态码为0，提示信息为：操作成功！
+     * 若没有数据返回，默认状态码为200，提示信息为：操作成功！
      */
     public JsonResult() {
-        this.code = "0";
+        this.code = "200";
         this.msg = "操作成功！";
     }
 
@@ -24,12 +24,12 @@ public class JsonResult<T> {
     }
 
     /**
-     * 有数据返回时，状态码为0，默认提示信息为：操作成功！
+     * 有数据返回时，状态码为200，默认提示信息为：操作成功！
      * @param data
      */
     public JsonResult(T data) {
         this.data = data;
-        this.code = "0";
+        this.code = "200";
         this.msg = "操作成功！";
     }
 
@@ -40,13 +40,13 @@ public class JsonResult<T> {
     }
 
     /**
-     * 有数据返回，状态码为0，人为指定提示信息
+     * 有数据返回，状态码为200，人为指定提示信息
      * @param data
      * @param msg
      */
     public JsonResult(T data, String msg) {
         this.data = data;
-        this.code = "0";
+        this.code = "200";
         this.msg = msg;
     }
     // 省略get和set方法
